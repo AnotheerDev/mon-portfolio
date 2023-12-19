95eb5fea29019a797b92cb17f5f26e9b4d33d240
AnotheerDev <leger.julien94@gmail.com>
2023-12-19 21:17:15 +0100

    Add Projects component and its CSS file

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './projects.css';
+
+const Projects = () => {
+    return (
+        <div className="projects-container">
+            <h1>Projets</h1>
+            <p>Voici quelques-uns de mes projets récents...</p>
+            {/* Plus de contenu ici */}
+        </div>
+    );
+}
+
+export default Projects;
