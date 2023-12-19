ca64123bfefc82b11665d80a9b0de2cd5432a3e4
AnotheerDev <leger.julien94@gmail.com>
2023-12-19 21:17:21 +0100

    Add Resume component and its CSS file

---
+++ src/components/Resume/Resume.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './resume.css';
+
+const Resume = () => {
+    return (
+        <div className="resume-container">
+            <h1>CV</h1>
+            <p>Voici mon CV...</p>
+            {/* Plus de contenu ici */}
+        </div>
+    );
+}
+
+export default Resume;
