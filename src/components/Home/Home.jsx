868e7c7ffd761e62cd783e655b13d1f529180cd8
AnotheerDev <leger.julien94@gmail.com>
2023-12-19 21:17:09 +0100

    Add Home component and its CSS file

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+// Home.jsx
+import React from 'react';
+import './home.css'; // Assurez-vous que le chemin est correct
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Bienvenue sur mon Portfolio</h1>
+      <p>Voici quelques-uns de mes projets récents...</p>
+      {/* Plus de contenu ici */}
+    </div>
+  );
+}
+
+export default Home;
