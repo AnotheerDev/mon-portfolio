0a57827b2870277764ab8779857128a86b170f7d
AnotheerDev <leger.julien94@gmail.com>
2023-12-19 23:00:06 +0100

    Add Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <a href="https://www.linkedin.com/in/julien-l%C3%A9ger/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
+      <a href="https://github.com/AnotheerDev" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+      </footer>
+  );
+}
+
+export default Footer;
