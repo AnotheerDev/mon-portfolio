c21e7c72981a33d2e9b75deef0c1c6c0abb47aea
AnotheerDev <leger.julien94@gmail.com>
2023-12-19 21:44:35 +0100

    Refactor Resume component and add CSS styling

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+// Resume.jsx
+import React from 'react';
 import './resume.css';
 
 const Resume = () => {
-    return (
-        <div className="resume-container">
-            <h1>CV</h1>
-            <p>Voici mon CV...</p>
-            {/* Plus de contenu ici */}
-        </div>
-    );
+  return (
+    <section id="resume">
+      <h1>CV</h1>
+      {/* Contenu pour votre CV, comme l'éducation, l'expérience de travail, les compétences, etc. */}
+    </section>
+  );
 }
 
 export default Resume;
