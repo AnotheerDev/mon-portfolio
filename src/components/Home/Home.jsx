e0c608a126f2e155cdbd68609d22499697be9365
AnotheerDev <leger.julien94@gmail.com>
2023-12-19 21:44:21 +0100

    Refactor Home component and CSS
    This commit refactors the Home component and its associated CSS file. The changes include renaming the component, updating the section ID, and removing unnecessary code.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,14 @@
 // Home.jsx
+// Home.jsx
 import React from 'react';
-import './home.css'; // Assurez-vous que le chemin est correct
+import './home.css';
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <h1>Bienvenue sur mon Portfolio</h1>
-      <p>Voici quelques-uns de mes projets récents...</p>
-      {/* Plus de contenu ici */}
-    </div>
+    <section id="home">
+      <h1>Accueil</h1>
+      {/* Contenu de l'accueil */}
+    </section>
   );
 }
 
