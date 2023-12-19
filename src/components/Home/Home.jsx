69170f915d1e9ec5771062c170c62f9816af9486
AnotheerDev <leger.julien94@gmail.com>
2023-12-19 23:00:12 +0100

    Update home section in Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,9 +5,17 @@ import './home.css';
 
 const Home = () => {
   return (
-    <section id="home">
-      <h1>Accueil</h1>
-      {/* Contenu de l'accueil */}
+    <section id="home" className="home-section">
+      <div className="container">
+        <div className="intro">
+          <h1>Bienvenue sur Mon Portfolio</h1>
+          <p>Je suis JULIEN LEGER, un développeur web et web mobile. Je cherche actuellement à la recherche d'une alternance pour le CDA (Concepteur Développeur d'Applications) </p>
+        </div>
+        <div className="cta">
+          <a href="#projects" className="btn">Voir Mes Projets</a>
+          <a href="#resume" className="btn">CV</a>
+        </div>
+      </div>
     </section>
   );
 }
