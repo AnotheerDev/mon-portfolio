d5dbd2dc90d0ed4649e013b06f8414b50bb7ce92
AnotheerDev <leger.julien94@gmail.com>
2023-12-19 22:59:32 +0100

    Add Footer component to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Projects from './components/Projects/Projects';
 import Resume from './components/Resume/Resume';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 const App = () => {
@@ -23,6 +24,7 @@ const App = () => {
         <div ref={sections[1]}><Projects /></div>
         <div ref={sections[2]}><Resume /></div>
         {/* Vous pouvez ajouter plus de sections ici si nécessaire */}
+        <Footer />
       </main>
       <div className="navigation-buttons">
         {activeIndex > 0 && <button onClick={() => scrollToSection(activeIndex - 1)}>&uarr;</button>}
