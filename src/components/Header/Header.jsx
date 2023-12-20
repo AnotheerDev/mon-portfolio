b17a466b16f9213cfbab506803d27b7b5ad9fc79
AnotheerDev <leger.julien94@gmail.com>
2023-12-20 16:48:23 +0100

    Update header navigation styling and add social media icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,16 @@ import './header.css';
 const Header = () => {
     return (
         <header>
-            <nav>
-                <ul>
-                    <li><a href="#home" className="active">Accueil</a></li>
-                    <li><a href="#projects">Projets</a></li>
-                    <li><a href="#resume">CV</a></li>
+            <nav className='nav'>
+                <ul className="nav-links">
+                    <li className="center"><a href="#home">Accueil</a></li>
+                    <li className="upward"><a href="#projects">Projets</a></li>
+                    <li className="forward"><a href="#resume">CV</a></li>
                 </ul>
+                <div className='nav-icons'>
+                    <a href="https://www.linkedin.com/in/julien-l%C3%A9ger/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
+                    <a href="https://github.com/AnotheerDev" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+                </div>
             </nav>
         </header>
     );
