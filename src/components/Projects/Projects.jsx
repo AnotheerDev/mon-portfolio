240ead2112db1077080457b41c0b2d2308a75346
AnotheerDev <leger.julien94@gmail.com>
2023-12-20 19:21:08 +0100

    Remove unnecessary heading and update styles in Projects component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,7 +5,6 @@ import './projects.css';
 const Projects = () => {
     return (
         <section id="projects">
-            <h1>Projets</h1>
             {/* Ici, vous pouvez ajouter des cartes de projet, des images, des descriptions, etc. */}
         </section>
     );
