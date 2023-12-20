4ae375ebb913de1d5628488d5f5ecf49390bb91c
AnotheerDev <leger.julien94@gmail.com>
2023-12-20 19:21:12 +0100

    Remove CV title and update styling in Resume component

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -5,7 +5,6 @@ import './resume.css';
 const Resume = () => {
   return (
     <section id="resume">
-      <h1>CV</h1>
       {/* Contenu pour votre CV, comme l'éducation, l'expérience de travail, les compétences, etc. */}
     </section>
   );
