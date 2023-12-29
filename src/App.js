9b7bbe8722dde9a429d0bc09920d0f71ffe72497
AnotheerDev <leger.julien94@gmail.com>
2023-12-29 13:57:55 +0100

    Refactor scroll behavior in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-// App.js
-import React, {useState, useRef} from 'react';
+import React, { useState, useRef } from 'react';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Projects from './components/Projects/Projects';
@@ -9,41 +8,30 @@ import './App.css';
 
 const App = () => {
   const [activeIndex, setActiveIndex] = useState(0);
-  const sections = [useRef(null), useRef(null), useRef(null)]; // pour Home, Projects, Resume
+  const sections = [useRef(null), useRef(null), useRef(null), useRef(null), useRef(null)]; // pour Home, Projects, Resume, Footer
 
   const scrollToSection = (index) => {
     setActiveIndex(index);
-  
-    setTimeout(() => {
-      const headerOffset = document.querySelector('header').offsetHeight; // Hauteur du header
-      const topOfSection = sections[index].current.offsetTop; // Position Y de la section depuis le haut du document
-      const offsetPosition = topOfSection - headerOffset; // Soustrait la hauteur du header pour éviter le chevauchement
-  
-      window.scrollTo({
-        top: offsetPosition,
-        behavior: 'smooth'
-      });
-    }, 0);
+    const sectionElement = sections[index].current;
+    if (sectionElement) {
+      sectionElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
   };
-  
-  
 
   return (
     <div>
-      <Header />
       <main className='main'>
-        <div ref={sections[0]}><Home /></div>
-        <div ref={sections[1]}><Projects /></div>
-        <div ref={sections[2]}><Resume /></div>
-        {/* Vous pouvez ajouter plus de sections ici si nécessaire */}
-        <Footer />
+        <div ref={sections[0]}><Header /></div>
+        <div ref={sections[1]}><Home /></div>
+        <div ref={sections[2]}><Projects /></div>
+        <div ref={sections[3]}><Resume /></div>
+        <div ref={sections[4]}><Footer /></div>
       </main>
       <div className="navigation-buttons">
         {activeIndex > 0 && <button onClick={() => scrollToSection(activeIndex - 1)}>&uarr;</button>}
         {activeIndex < sections.length - 1 && <button onClick={() => scrollToSection(activeIndex + 1)}>&darr;</button>}
       </div>
     </div>
-    
   );
 }
 
