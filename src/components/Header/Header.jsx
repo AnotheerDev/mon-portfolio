263e796cf00cedad58f31033ab4d3317ce99a9d6
AnotheerDev <leger.julien94@gmail.com>
2023-12-31 16:14:45 +0100

    Add menu functionality to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
     return (
         <header>
             <nav className='nav'>
-                <ul className="nav-links">
-                    <li className="center"><a href="#home">Accueil</a></li>
-                    <li className="upward"><a href="#resume">CV</a></li>
-                    <li className="forward"><a href="#projects">Projets</a></li>
+                <div className="burger-menu" onClick={toggleMenu}>
+                    <i className="fas fa-bars"></i>
+                </div>
+                <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
+                    <li className="center" onClick={closeMenu}><a href="#home">Accueil</a></li>
+                    <li className="upward" onClick={closeMenu}><a href="#resume">CV</a></li>
+                    <li className="forward" onClick={closeMenu}><a href="#projects">Projets</a></li>
                 </ul>
                 <div className='nav-icons'>
                     <a href="https://www.linkedin.com/in/julien-l%C3%A9ger/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
